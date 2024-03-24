123860955a0e8b6b08ee5bd354e7f148b291efe0
Jesse C. Lin <jesse.c.lin@outlook.com>
2024-03-24 20:58:19 +0800

    More examples

--- real-world-cases/SERV/build/servant_1.2.1/verilator_tb/servant_tb.d
+++
@@ -1,7 +0,0 @@
-servant_tb.o: src/servant_1.2.1/bench/servant_tb.cpp \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_trace.h Vservant_sim.h \
- /usr/share/verilator/include/verilated_heavy.h Vservant_sim__Dpi.h \
- /usr/share/verilator/include/vltstd/svdpi.h
